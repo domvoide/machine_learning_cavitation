d177f2053c91e587156f715e870fc78b0cefcba7
domvoide <voide.dominique@gmail.com>
2020-03-02 20:28:02 +0100

    Comparaison scipy-sort

--- test_CDF.py
+++ test_CDF.py
@@ -4,23 +4,24 @@ Created on Mon Mar  2 13:15:20 2020
 
 @author: dominiqu.voide
 """
+
 import pickle
 import numpy as np
 import scipy
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-infile = open('Datas\\Short_2s_sample_1.8s_step.pckl', 'rb')
+infile = open('Datas\\Pickle\\Sampling\\Short_1s_sample_0.9s_step.pckl', 'rb')
 df = pickle.load(infile)
 infile.close()
 
 X = df.Micro
 
 norm_cdf_no_cavit = np.sort(X[0])
-norm_cdf_cavit = np.sort(X[148])
+norm_cdf_cavit = np.sort(X[322])
 
-p_no_cavit = 1. * np.arange(len(X[0])) / (len(X[0]) - 1)
-p_cavit = 1. * np.arange(len(X[148])) / (len(X[148]) - 1)
+p_no_cavit = np.linspace(0, 1, len(X[0]))
+p_cavit = np.linspace(0, 1, len (X[322]))
 
 # plot the sorted data:
 fig = plt.figure(figsize=(8,6))
@@ -29,6 +30,8 @@ fig.suptitle('Cumulative distribution function, CDF', y=1.02, fontsize=15)
 ax1 = fig.add_subplot(121)
 ax1.plot(p_no_cavit, norm_cdf_no_cavit)
 plt.title('No Cavitation')
+
+
 ax1.set_xlabel('$p$')
 ax1.set_ylabel('$x$')
 ax1.set_ylim(-3, 3)
@@ -41,6 +44,34 @@ ax2.set_ylabel('$x$')
 ax2.set_ylim(-3, 3)
 ax2.label_outer()
 
-
+print('Numpy Order')
 print('CDF pas de cavitation : '+ str(max(norm_cdf_no_cavit)))
 print('CDF cavitation : ' + str(max(norm_cdf_cavit)))
+
+norm_cdf_no_cavit2 = scipy.stats.norm.cdf(X[0])
+norm_cdf_cavit2 = scipy.stats.norm.cdf(X[322])
+
+fig = plt.figure(figsize=(8,6))
+fig.suptitle('CDF with scipy stats norm', y=1.02, fontsize=15)
+
+ax1 = fig.add_subplot(121)
+sns.lineplot(x=X[0], y=norm_cdf_no_cavit2, ci=None)
+plt.title('No Cavitation')
+
+
+ax1.set_xlabel('$p$')
+ax1.set_ylabel('$x$')
+ax1.set_ylim(-3, 3)
+
+ax2 = fig.add_subplot(122)
+sns.lineplot(x=X[322], y=norm_cdf_cavit2, ci=None)
+plt.title('Cavitation')
+ax2.set_xlabel('$p$')
+ax2.set_ylabel('$x$')
+ax2.set_ylim(-3, 3)
+ax2.label_outer()
+
+plt.show()
+print('Scipy stats norm CDF')
+print('CDF pas de cavitation : '+ str(max(norm_cdf_no_cavit2)))
+print('CDF cavitation : ' + str(max(norm_cdf_cavit2)))
