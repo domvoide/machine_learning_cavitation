9dcaef6865cf92d5e391eec1904f79fecc0b2412
domvoide <voide.dominique@gmail.com>
2020-03-03 13:55:35 +0100

    Update 3_Features.py

--- 3_Features.py
+++ 3_Features.py
@@ -18,31 +18,35 @@ infile = open('Datas\\Pickle\\Sampling\\' + filename + '.pckl', 'rb')
 df = pickle.load(infile)
 infile.close()
 
+
 def rms(audio):
     return sqrt(sum(n*n for n in audio)/len(audio))
 
+
 def variance(audio):
     return statistics.variance(audio)
+
 
 def maxPSD(audio):
     freqs, psd = signal.welch(audio)
     maxPSD = max(psd)
     return maxPSD
-    
-    
+
+
 def fftfreq(audio, Fs):
     L = len(audio)
     NFFT = int(2 ** np.ceil(np.log2(abs(L))))
-    freq = Fs / 2 * np.linspace(0,1,NFFT//2)
-    
+    freq = Fs / 2 * np.linspace(0, 1, NFFT//2)
+
     yf = np.fft.fft(audio, NFFT)/L
     amp = 2 * abs(yf[1:NFFT//2+1])
-      
+
     # Déterminer les 10 peaks les plus importants
     freqmax_index = abs(np.argsort(-amp[100:NFFT//2])[:10]) # néglige les fréquences en dessous de 100 Hz
     freqmax = np.sort(freq[freqmax_index])[::-1]
     return freqmax
- 
+
+
 RMS = []
 var = []
 PSD = []
@@ -76,24 +80,24 @@ for i in range(len(df.Micro)):
     fft8.append(fft_mat[8])
     fft9.append(fft_mat[9])
 
-data = {'RMS' : RMS,
-        'Var' : var,
-        'PSD' : PSD,
-        'fft0' : fft0,
-        'fft1' : fft1,
-        'fft2' : fft2,
-        'fft3' : fft3,
-        'fft4' : fft4,
-        'fft5' : fft5,
-        'fft6' : fft6,
-        'fft7' : fft7,
-        'fft8' : fft8,
-        'fft9' : fft9
+data = {'RMS': RMS,
+        'Var': var,
+        'PSD': PSD,
+        'fft0': fft0,
+        'fft1': fft1,
+        'fft2': fft2,
+        'fft3': fft3,
+        'fft4': fft4,
+        'fft5': fft5,
+        'fft6': fft6,
+        'fft7': fft7,
+        'fft8': fft8,
+        'fft9': fft9
         }
 
 X = pd.DataFrame(data)
 y = df.Cavit
 
-g = open('Datas\\Pickle\\Features\\Features' + filename + '.pckl', 'wb')
-pickle.dump((X,y), g)
+g = open('Datas\\Pickle\\Features\\Features_' + filename + '.pckl', 'wb')
+pickle.dump((X, y), g)
 g.close()
