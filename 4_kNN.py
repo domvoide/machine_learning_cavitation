5da952db06c7ca0cd1ef6bc82e3a71196f959332
domvoide <voide.dominique@gmail.com>
2020-03-05 23:05:52 +0100

    Update 4_kNN.py

--- 4_kNN.py
+++ 4_kNN.py
@@ -24,37 +24,68 @@ infile.close()
 X = df[0]
 y = df[1]
 
-sel = VarianceThreshold(threshold=(.8 * (1 - .8)))
-X2 = sel.fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3) # 70% training and 30% test
 
-X_train, X_test, y_train, y_test = train_test_split(X2, y, test_size=0.3) # 70% training and 30% test
-
+kmax = int(np.sqrt(len(y)))
+euclid = {'k': [], 'errortrain': [], 'errortest': []}
+manathan = {'k': [], 'errortrain': [], 'errortest': []}
 
 # création du modèle kNN
-knn = KNeighborsClassifier(n_neighbors=6, p=1)
-
+knn = KNeighborsClassifier(n_neighbors=int(np.sqrt(len(y)))) # p = 1 for manhattan distance
+print('Facteur k = ' + str(kmax) + ' et distance euclidienne')
 # Train the model using the training sets
 knn.fit(X_train, y_train)
-
+errortrain = 1 - knn.score(X_train, y_train)
 y_pred = knn.predict(X_test)
-error = 1 - knn.score(X_test, y_test)
-print('Erreur: %f' % error)
+errortest = 1 - knn.score(X_test, y_test)
+print('Erreur train: %f' % errortrain)
+print('Erreur test: %f' % errortest)
 print("Accuracy:" + str(metrics.accuracy_score(y_test, y_pred)))
 
-errors = []
-for k in range(2, 200):
-    knn = neighbors.KNeighborsClassifier(k)
-    errors.append(1 - knn.fit(X_train, y_train).score(X_test, y_test))
-indexmin = errors.index(min(errors)) + 2
-plt.plot(range(2, 200), errors, 'o-')
-plt.xlabel('Number of neighbors')
-plt.ylabel('Error')
-plt.show()
 
-knn = neighbors.KNeighborsClassifier(indexmin)
-print('Facteur k choisi : ' + str(indexmin))
-print('Erreur : ' + str(min(errors)))
-knn.fit(X_train, y_train)
-
-
-print("Accuracy:" + str(metrics.accuracy_score(y_test, y_pred)))
+for i in range(10):
+    errorstrain = []
+    errorstest = []
+    print('\nMesure distance Euclidienne')
+    for k in range(2, kmax):
+        knn = neighbors.KNeighborsClassifier(n_neighbors=k, p=2)
+        errorstrain.append(1 - knn.fit(X_train, y_train).score(X_train, y_train))
+        errorstest.append(1 - knn.fit(X_train, y_train).score(X_test, y_test))
+    indexmin = errorstest.index(min(errorstest)) + 2
+    Errormintrain = min(errorstrain)
+    Errormintest = min(errorstest)
+    # plt.plot(range(2, kmax), errorstest, 'o-')
+    # plt.xlabel('Number of neighbors')
+    # plt.ylabel('Error')
+    # plt.show()
+    
+    knn = neighbors.KNeighborsClassifier(indexmin)
+    print('Facteur k choisi : ' + str(indexmin))
+    print('Min erreur train: ' + str(min(errorstrain)))
+    print('Erreur test avec k choisi: ' + str(min(errorstest)))
+    euclid['k'].append(indexmin)
+    euclid['errortrain'].append(Errormintrain)
+    euclid['errortest'].append(Errormintest)
+    
+    print('\nMesure distance manhattan')
+    errorstrain = []
+    errorstest = []
+    for k in range(2, kmax):
+        knn = neighbors.KNeighborsClassifier(n_neighbors=k, p=1)
+        errorstrain.append(1 - knn.fit(X_train, y_train).score(X_train, y_train))
+        errorstest.append(1 - knn.fit(X_train, y_train).score(X_test, y_test))
+    indexmin = errorstest.index(min(errorstest)) + 2
+    Errormintrain = min(errorstrain)
+    Errormintest = min(errorstest)
+    # plt.plot(range(2, kmax), errorstest, 'o-')
+    # plt.xlabel('Number of neighbors')
+    # plt.ylabel('Error')
+    # plt.show()
+    
+    knn = neighbors.KNeighborsClassifier(indexmin)
+    print('Facteur k choisi : ' + str(indexmin))
+    print('Min erreur train: ' + str(min(errorstrain)))
+    print('Erreur test avec k choisi: ' + str(min(errorstest)))
+    manathan['k'].append(indexmin)
+    manathan['errortrain'].append(Errormintrain)
+    manathan['errortest'].append(Errormintest)
