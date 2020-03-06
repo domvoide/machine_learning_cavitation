b84803af09e5858b0db27c8e68f8f4db423f9849
domvoide <voide.dominique@gmail.com>
2020-03-06 13:24:22 +0100

    Update test_sinus_fft.py

--- test_sinus_fft.py
+++ test_sinus_fft.py
@@ -20,7 +20,7 @@ N = 600
 
 T = 1.0 / 800.0 
 x = np.linspace(0.0, N*T, N) 
-y = np.sin(50.0 * 2.0*np.pi*x) + 0.5*np.sin(200.0 * 2.0*np.pi*x)
+y = 3*np.sin(5.0 * 2.0*np.pi*x) + 5*np.sin(50.0 * 2.0*np.pi*x)
 yf = scipy.fftpack.fft(y) 
 xf = np.linspace(0.0, 1.0/(2.0*T), N//2) 
 
@@ -30,8 +30,9 @@ ax.plot(x,y)
 ax1 = fig.add_subplot(212)
 ax1.plot(xf, 2.0/N * np.abs(yf[:N//2])) 
 ax1.label_outer()
-peaks, _ = find_peaks(yf, height=0.3)
+# peaks, _ = find_peaks(yf, height=0.3)
 
+#freqmax_index = argrelextrema(yf[0:N//2], np.greater)[0]
 freqmax_index = np.where(yf == max(yf[0:N//2]))
 
 freqmax = xf[freqmax_index]
