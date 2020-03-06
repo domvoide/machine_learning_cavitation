5aa2d5de7a475f3fada34867061fd4ee1c05c157
domvoide <voide.dominique@gmail.com>
2020-03-06 13:24:19 +0100

    Update test_fft.py

--- test_fft.py
+++ test_fft.py
@@ -7,7 +7,7 @@ Created on Fri Feb 28 07:57:07 2020
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import find_peaks_cwt
+
 
 infile = open('Datas\\Pickle\\Sampling\\Short_1s_sample_0.9s_step.pckl', 'rb')
 df = pickle.load(infile)
@@ -35,5 +35,5 @@ plt.ylabel('Amplitude [-]')
 
 # Déterminer les 10 peak les plus importants
 # freqmax_index = abs(np.argsort(-amp[100:NFFT//2])[:10])
-freqmax_index = np.where(yf == max(yf[0:N//2]))
-freqmax = np.sort(freq[freqmax_index])[::-1]
+freqmax_index = np.where(yf == max(yf[0:NFFT//2]))
+freqmax = freq[freqmax_index]
