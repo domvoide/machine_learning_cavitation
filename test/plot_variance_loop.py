22d69ec7a8fb9e85945d1a27a3d2366274bc14e6
domvoide <voide.dominique@gmail.com>
2020-03-07 16:14:01 +0100

    Update plot_variance_loop.py
    Plot de toutes les variances et de la moyenne par label

--- test/plot_variance_loop.py
+++ test/plot_variance_loop.py
@@ -4,59 +4,56 @@ Created on Fri Mar  6 16:29:06 2020
 
 @author: voide
 
-plot de toutes les variances
+plot de toutes les variances et de la moyenne de chaque label
 """
-import os
+
+import numpy as np
 import pickle
 import statistics
 from matplotlib import pyplot as plt
 from datetime import datetime
 
 t0 = datetime.now()
-filename = 'Short_1s_sample_0.9s_step'
+filename = '1s_sample_0.1s_ti'
 # read pickle file
 
 infile = open('..\\Datas\\Pickle\\Sampling\\' + filename + '.pckl', 'rb')
 df = pickle.load(infile)
 infile.close()
 
+cavit = []
+no_cavit = []
 
 fig = plt.figure(figsize=(10,6))  # création figure
-
-def plotVar(audio, y, color):
-    '''
-    Fonction pour plotter un point pour la variance d'un signal'
-    Parameters
-    ----------
-    audio : array
-        Signal audio à analyser.
-    y : int
-        position en y du plot 1D.
-    color : str
-        couleur du point en fonction du label.
-
-    Returns
-    -------
-    Scatter de la variance
-
-    '''
-    var = statistics.variance(audio)
-    plt.scatter(var, y, color=color)
 
 # Boucle pour plotter toutes les variances
 for i in range(len(df.Micro)):
     if df.Cavit[i] == 0:
         c = 'b'
         y = 0
+        var = statistics.variance(df.Micro[i])
+        plt.scatter(var, y, color=c)
+        no_cavit.append(var)
     else:
         c = 'r'
         y = 1
-    plotVar(df.Micro[i], y, c)
+        var = statistics.variance(df.Micro[i])
+        plt.scatter(var, y, color=c)
+        cavit.append(var)
     
 # Mise en forme du graphique
-plt.text(1.6, 1, 'Red : cavitation\nBlue : no cavitation', fontsize = 15)
+plt.text(1.6, 0.8, 'Red : cavitation\nBlue : no cavitation\nx : mean', fontsize = 15)
 plt.xlabel('Variance [-]')
 plt.ylabel('Cavitation [-]')
 
+# moyenne des variances par label
+mean_no_cavit = np.mean(no_cavit)
+mean_cavit = np.mean(cavit)
+
+# plot des moyennes
+plt.scatter(mean_no_cavit, 0, c='black', marker='x', s=100)
+plt.scatter(mean_cavit, 1, c='black', marker='x', s=100)
+
+#affichage du temps écoulé
 t1 = datetime.now()
-print('Temps écoulé : ' + (t1-t0))
+print('Temps écoulé : ' + str(t1-t0))
