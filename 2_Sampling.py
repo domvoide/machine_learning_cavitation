aa78d4b6e89a06631804a9ad3c6a6ac7b13a5c84
domvoide <voide.dominique@gmail.com>
2020-03-12 18:32:22 +0100

    Update 2_Sampling.py
    Récupération de tous les vecteurs

--- 2_Sampling.py
+++ 2_Sampling.py
@@ -22,22 +22,28 @@ ti = 0  # durée de recouvrement en secondes (pas entre chaque échantillon)
 # importation du pickle contenant les 18 fichiers de données
 folder_path = 'Datas\\Pickle\\all_data.pckl'
 buf = io.open(folder_path, 'rb', buffering=(128 * 2048))
-dataframe = pickle.load(buf)
+df = pickle.load(buf)
 buf.close()
 del buf
-dataframe = dataframe.transpose()
+df = df.transpose()
 
-# creation d'une matrice plus compact pour commencer (plus rapide à travailler)
-df2 = dataframe[['Alpha', 'Sigma', 'Time', 'Micro', 'Cavit']].copy()
-del dataframe
+# # creation d'une matrice plus compact pour commencer (plus rapide à travailler)
+# df2 = dataframe[['Alpha', 'Sigma', 'Time', 'Micro', 'Cavit']].copy()
+# del dataframe
 
 # Acquisition de la fréquence
-f = round(1 / (df2.Time[0][1]-df2.Time[0][0]), 1)
+f = round(1 / (df.Time[0][1]-df.Time[0][0]), 1)
 
 # Création d'un dictionnaire vide pour les échantillons d'analyses
 data = {'Alpha': [],
         'Sigma': [],
+        'Acc_X': [],
+        'Acc_Y': [],
+        'Acc_Z': [],
         'Micro': [],
+        'Uni': [],
+        'Hydro': [],
+        'AE': [],
         'Cavit': []}
 
 size = int(dt * f)  # longueur de l'échantillon
@@ -45,13 +51,17 @@ step = int((1-ti) * f)  # écart entre le début de chaque échantillon
 
 # création des échantillons
 for j in range(18):
-    for i in range(0, len(df2.Micro[j]), step):
-        a = df2.Micro[j][i: i + size]
-        a = a.astype('float64')
-        data['Micro'].append(a)
-        data['Cavit'].append(df2.Cavit[j])
-        data['Alpha'].append(df2.Alpha[j])
-        data['Sigma'].append(df2.Sigma[j])
+    for i in range(0, len(df.Micro[j]), step):
+        data['Acc_X'].append(df.Acc_X[j][i: i + size])
+        data['Acc_Y'].append(df.Acc_Y[j][i: i + size])
+        data['Acc_Z'].append(df.Acc_Z[j][i: i + size])
+        data['Micro'].append(df.Micro[j][i: i + size])
+        data['Uni'].append(df.Uni[j][i: i + size])
+        data['Hydro'].append(df.Hydro[j][i: i + size])
+        data['AE'].append(df.Brut[j][i: i + size])
+        data['Cavit'].append(df.Cavit[j])
+        data['Alpha'].append(df.Alpha[j])
+        data['Sigma'].append(df.Sigma[j])
 
 
 # Création d'une liste des échantillons de la mauvaise taille
@@ -64,20 +74,21 @@ dellist = dellist[::-1]  # inversion du sens de la liste
 # Suppression des échantillons de mauvaise taille
 for nb in dellist:
     print(nb)
+    del data['Acc_X'][nb]
+    del data['Acc_Y'][nb]
+    del data['Acc_Z'][nb]
     del data['Micro'][nb]
+    del data['Uni'][nb]
+    del data['Hydro'][nb]
+    del data['AE'][nb]
     del data['Cavit'][nb]
     del data['Alpha'][nb]
     del data['Sigma'][nb]
-
-# transformation en dataframe et mise en forme pour compacter
-df = pd.DataFrame(data)
-df.Cavit = df.Cavit.astype('uint8')
-df.Alpha = df.Alpha.astype('float32')
-df.Sigma = df.Sigma.astype('float32')
+    del data['Sigma'][nb]
 
 # Affichage du graphe des points de fonctionnements testés
 fig, ax = plt.subplots(figsize=(11, 8))
-scatter = ax.scatter(df2.Alpha, df2.Sigma, c=df2.Cavit, cmap='RdYlBu')
+scatter = ax.scatter(df.Alpha, df.Sigma, c=df.Cavit, cmap='RdYlBu')
 legend1 = ax.legend(*scatter.legend_elements(),
                     loc="upper left", title="Cavitation")
 ax.add_artist(legend1)
@@ -86,8 +97,17 @@ plt.ylabel(r'$\sigma$ [-]')
 plt.grid()
 plt.show()
 
+    
+del df
+# transformation en dataframe et mise en forme pour compacter
+df = pd.DataFrame(data)
+# df.Cavit = df.Cavit.astype('uint8')
+# df.Alpha = df.Alpha.astype('float32')
+# df.Sigma = df.Sigma.astype('float32')
+
+
 # enregistrement en pickle
-g = open('Datas\\Pickle\\Sampling\\' + str(dt) + 's_sample_' + str(ti) +
+g = open('Datas\\Pickle\\Sampling\\All_' + str(dt) + 's_sample_' + str(ti) +
          's_ti.pckl', 'wb')
 pickle.dump(df, g)
 g.close()
